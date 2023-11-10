3f96b20fccf39ec19973f4a3573c44df22a77844
Ali Mohamed <150080109+alimobasher@users.noreply.github.com>
2023-11-10 17:31:12 +0200

    Add files via upload

---
+++ Task2_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float R,A;
+    printf("Enter radius: \n");
+    scanf("%f",&R);
+
+    A = (22.0/7.0)*R*R;
+    printf("The Area is %f",A);
+
+
+}
