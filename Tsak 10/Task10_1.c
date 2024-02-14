6d8dacbb962f535e1e9600f264508dd44196ef9d
alimobasher <150080109+alimobasher@users.noreply.github.com>
2024-02-14 17:12:41 +0200

    Defines an int initialized

---
+++ Tsak 10/Task10_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int NumberOne = 10;
+unsigned int *Ptr;
+
+int main()
+{
+    printf("NumberOne Value = %i\n\n" ,NumberOne);
+
+    Ptr = &NumberOne;
+
+    *Ptr = 20;
+    printf("NumberOne Value edit = %i\n" ,NumberOne);
+
+
+    return 0;
+}
