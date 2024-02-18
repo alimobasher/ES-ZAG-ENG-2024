c7695f1ba73fbab5c00689dd9152b6f699d122d8
alimobasher <150080109+alimobasher@users.noreply.github.com>
2024-02-18 20:53:40 +0200

    Pointer

--- Tsak 10/Task10_1.c
+++ Tsak 10/Task10_1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int Number = 10;
+unsigned int *Ptr_Number = &Number;
+
+
+int main()
+{
+   printf("The Number is =%i\n\n",Number);
+
+   *Ptr_Number = 20;
+
+   printf("The Number after edit  is =%i\n",Number);
+
+
+
+    return 0;
+}
 
